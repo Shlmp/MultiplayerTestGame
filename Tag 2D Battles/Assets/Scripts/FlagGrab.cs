131e8add3b64ead5d0023ef204ad76131d78f588
Shlmp <127421834+Shlmp@users.noreply.github.com>
2025-10-23 15:38:05 -0600

    Add Feature: Grab Flags
    Added the ability to grab flags, needs fixing. Also, fixed player movement, still choppy.

--- Tag 2D Battles/Assets/Scripts/FlagGrab.cs
+++ Tag 2D Battles/Assets/Scripts/FlagGrab.cs
@@ -1,16 +1,33 @@
 using UnityEngine;
+using Fusion;
 
-public class FlagGrab : MonoBehaviour
+public class FlagGrab : NetworkBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private GameObject player;
+    private PlayerInputs playerInputs;
+
+    private void Awake()
     {
-        
+        playerInputs = new PlayerInputs();
+        playerInputs.Player.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        player = collision.gameObject;
+        Debug.Log(player + "  --  Detected");
+
+        if (player != null)
+        {
+            Debug.Log("Parent is now  --  " + player);
+            RPC_GrabFlag();
+        }
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.StateAuthority)]
+    private void RPC_GrabFlag()
+    {
+            transform.SetParent(player.transform);
+            transform.position = new Vector2(transform.parent.position.x, transform.parent.position.y + 1);
     }
 }
--- Tag 2D Battles/Assets/Scripts/PlayerMovement.cs
+++ Tag 2D Battles/Assets/Scripts/PlayerMovement.cs
@@ -1,7 +1,10 @@
 using Fusion;
+using Fusion.Sockets;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : NetworkBehaviour
+public class PlayerMovement : NetworkBehaviour, INetworkRunnerCallbacks
 {
     public float speed = 2f;
     private Rigidbody2D rb;
@@ -13,10 +16,14 @@ public class PlayerMovement : NetworkBehaviour
         col = GetComponent<BoxCollider2D>();
     }
 
-    public override void FixedUpdateNetwork()
+    public void OnInput()
     {
         if (GetInput<MyInput>(out var inputs) == false) { return; }
+    }
 
+    public override void FixedUpdateNetwork()
+    {
+        OnInput();
         Debug.Log("Is Moving");
         Movement();
     }
@@ -27,7 +34,106 @@ public class PlayerMovement : NetworkBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
         Debug.Log("MOVE, Speed is  -  " + speed);
-        Vector2 moveVelocity = new Vector2(horizontalInput * speed * Runner.DeltaTime, verticalInput * speed * Runner.DeltaTime);  //  NEEDS TO BE FIXED
+        
+        Vector2 moveVelocity = new Vector2(horizontalInput * speed, verticalInput * speed);  //  NEEDS TO BE FIXED
+       
         rb.linearVelocity = moveVelocity;
     }
+
+    #region Inputs
+    public void OnObjectExitAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnObjectEnterAOI(NetworkRunner runner, NetworkObject obj, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPlayerJoined(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnPlayerLeft(NetworkRunner runner, PlayerRef player)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnShutdown(NetworkRunner runner, ShutdownReason shutdownReason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnDisconnectedFromServer(NetworkRunner runner, NetDisconnectReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectRequest(NetworkRunner runner, NetworkRunnerCallbackArgs.ConnectRequest request, byte[] token)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectFailed(NetworkRunner runner, NetAddress remoteAddress, NetConnectFailedReason reason)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnUserSimulationMessage(NetworkRunner runner, SimulationMessagePtr message)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataReceived(NetworkRunner runner, PlayerRef player, ReliableKey key, ArraySegment<byte> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnReliableDataProgress(NetworkRunner runner, PlayerRef player, ReliableKey key, float progress)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInput(NetworkRunner runner, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnInputMissing(NetworkRunner runner, PlayerRef player, NetworkInput input)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnConnectedToServer(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSessionListUpdated(NetworkRunner runner, List<SessionInfo> sessionList)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnCustomAuthenticationResponse(NetworkRunner runner, Dictionary<string, object> data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnHostMigration(NetworkRunner runner, HostMigrationToken hostMigrationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadDone(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void OnSceneLoadStart(NetworkRunner runner)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
 }
